07519a4c50555f975c729beed63cfbeca49293f9
Soumik Sen <mastersoumik2003@gmail.com>
2025-01-08 13:03:58 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -26,6 +26,12 @@ app.use(
   })
 );
 
+app.use(cors({
+  origin: 'https://vox-debate.vercel.app/',
+  credentials: true,
+}));
+
+
 // Configurations for different types of data acceptance
 app.use(express.json());
 app.use(express.urlencoded({ extended: true, limit: "20kb" }));
